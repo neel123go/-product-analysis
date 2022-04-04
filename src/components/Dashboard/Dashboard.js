d4e75337541dfb302dc845becd783bbe2c947234
neel123go <ayounpaul1029@gmail.com>
2022-04-04 14:28:42 +0600

    dashboard page responsive done

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Area, AreaChart, CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis } from 'recharts';
+import { Area, AreaChart, CartesianGrid, Legend, Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts';
 
 const Dashboard = () => {
     const [data, setData] = useState([]);
@@ -10,12 +10,13 @@ const Dashboard = () => {
             .then(data => setData(data));
     }, []);
     return (
-        <div className='mt-20 grid grid-cols-2 px-32 gap-60'>
-            <div className='line-chart'>
+        <div className='mt-20 grid grid-cols-1 lg:grid-cols-2 xl:px-32 gap-10 xl:gap-60'>
+            <div>
                 <h2 className='text-blue-600 mb-5 text-xl font-semibold'>MONTH WISE SELL</h2>
                 <LineChart
-                    width={500}
-                    height={400}
+                    className='mx-auto'
+                    width={300}
+                    height={280}
                     data={data}
                 >
                     <CartesianGrid strokeDasharray="3 3" />
@@ -25,12 +26,14 @@ const Dashboard = () => {
                     <Legend />
                     <Line type="monotone" dataKey="sell" stroke="#8884d8" activeDot={{ r: 8 }} />
                 </LineChart>
+
             </div>
-            <div className='bar-chart'>
+            <div>
                 <h2 className='text-blue-600 mb-5 text-xl font-semibold'>INVESTMENT VS REVENUE</h2>
                 <AreaChart
-                    width={500}
-                    height={400}
+                    className='mx-auto'
+                    width={300}
+                    height={280}
                     data={data}
                 >
                     <CartesianGrid strokeDasharray="3 3" />
