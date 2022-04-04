4930445c8e2ee8baefea274a299c6cde91c8a1a4
neel123go <ayounpaul1029@gmail.com>
2022-04-04 17:36:18 +0600

    about page update and responsive completed

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,7 +3,8 @@ import React from 'react';
 const About = () => {
     return (
         <div>
-            <h2>This is about</h2>
+            <h2 className='text-4xl text-blue-500 text-center mt-20'>Arora Shoes</h2>
+            <p className='text-center mt-5 px-5 lg:w-1/2 mx-auto text-slate-500'>Arora Shoes is a footwear manufacturing company that is engaged in the design, development, manufacturing, and worldwide marketing and sales of footwear, apparel, equipment, accessories, and services. The company is headquartered near Sylhet in the Bangladesh area.</p>
         </div>
     );
 };
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -21,7 +21,7 @@ const Home = () => {
                 </div>
             </div>
 
-            <div className="px-0 md:px-10 lg:px-40 max-w-7xl mx-auto">
+            <div className="px-0 md:px-8 lg:px-28">
                 <h2 className='text-slate-800 text-3xl md:text-4xl lg:mt-24'>Customer Reviews({reviews.length})</h2>
                 <div className='grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-10 md:gap-20 xl:gap-32 mt-14'>
                     {
